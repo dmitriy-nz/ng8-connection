f5a680e8319ef9608ee85a59e269a1ea8f2f5295
eggplant <eggplant665@outlook.com>
2019-06-27 10:56:37 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng8-connection-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Ng8ConnectionLibrary';
+}
